44e6736784f5dee18f6d1acb922b358367906fca
Frischid <d.88299@web.de>
2023-05-09 23:26:43 +0100

    Change Navbar to get access with less clicks

--- CaSSAndRA/src/components/navbar.py
+++ CaSSAndRA/src/components/navbar.py
@@ -6,26 +6,40 @@ import dash_bootstrap_components as dbc
 from . import ids
 
 # component
-navbar = dbc.NavbarSimple(
-        children=[
-            dbc.NavItem(dbc.NavLink('Home', href='/')),
-            dbc.DropdownMenu(
-                children=[
-                    #dbc.DropdownMenuItem('More pages', header=True),
-                    #dbc.DropdownMenuItem('Taskplanner', href='/taskplanner'),
-                    #dbc.DropdownMenuItem('Statistik', href='/stats'),
-                    dbc.DropdownMenuItem('Mapping', href='/mapping'),
-                    dbc.DropdownMenuItem('Settings', href='/settings'),
-                ],
-                nav=True,
-                in_navbar=True,
-                label="More",
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            dbc.NavbarBrand("CaSSAndRA",href="/"),
+            dbc.Nav(dbc.NavItem([
+                dbc.Button(id=ids.OPEN_OFFCANVAS, n_clicks=0, size='lg',class_name='bi bi-joystick', title='Open Remote Control'),
+                dbc.Button(id=ids.BUTTONOPENMODALINFO, n_clicks=0, size='lg',class_name='bi bi-info-square ms-3 me-5', title='Open Infobox')
+            ]), className="ms-auto"),
+
+            dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
+            dbc.Collapse(
+                dbc.Nav([
+                    dbc.NavItem(dbc.NavLink('Mapping', href='/mapping')),
+                    dbc.NavItem(dbc.NavLink('Settings', href='/settings')),
+                    #dbc.NavItem(dbc.NavLink('Taskplanner', href='/taskplanner')),
+                    #dbc.NavItem(dbc.NavLink('Statistik', href='/stats')),
+                ]),
+            id="navbar-collapse",
+            is_open=False,
+            navbar=True,
             ),
-            dbc.Button(id=ids.OPEN_OFFCANVAS, n_clicks=0, size='lg',class_name='bi bi-joystick ms-3', title='Open Remote Control'),
-            dbc.Button(id=ids.BUTTONOPENMODALINFO, n_clicks=0, size='lg',class_name='bi bi-info-square ms-3', title='Open Infobox')
-        ],
-        brand="CaSSAndRA",
-        brand_href="/",
-        color="primary",
-        dark=True,
-    )
+        ]),
+    className="mb-2",
+    color="primary",
+    dark=True,
+)
+
+# add callback for toggling the collapse on small screens
+@callback(
+    Output("navbar-collapse", "is_open"),
+    [Input("navbar-toggler", "n_clicks")],
+    [State("navbar-collapse", "is_open")],
+)
+def toggle_navbar_collapse(n, is_open):
+    if n:
+        return not is_open
+    return False
