001ff672a9d81e8fd14d599978c6950e36e5de19
Alexander Weiss <sanjok@gmx.de>
2023-06-05 09:31:26 +0200

    Cleaning httpcomm.py: - Remove unneccassaries imports - Added some new logger lines

--- CaSSAndRA/src/backend/comm/httpcomm.py
+++ CaSSAndRA/src/backend/comm/httpcomm.py
@@ -1,13 +1,13 @@
 import logging
 logger = logging.getLogger(__name__)
 
-import json
+#package imports
 import requests
 import time
-import pandas as pd
 
-from src.backend.data import datatodf, roverdata
-from . import cmdtorover, cmdlist, message
+#locale imports
+from src.backend.data import datatodf
+from . import message
 
 Headers = {"Content-Type": "text/plain"}
 
@@ -49,28 +49,34 @@ def cmd_to_rover(connect_data: dict(), connection: list()):#, msg_pckg: pd.DataF
         logger.debug('Backend: '+msg_pckg['msg'][i]+' is send to the rover')
         data = reqandchecksum(msg_pckg['msg'][i])
         if connection[1] == 1:
-            logger.debug('encryption true')
+            logger.debug('Encryption: true')
             data_ascii = [ord(c) for c in data]
             data_encrypt = [x + encryptkey for x in data_ascii]
             data_encrypt = [x - 126 + 31 if x>=126 else x for x in data_encrypt]
             data = ''.join(map(chr, data_encrypt))  
-
-        res = requests.post(url=connect_data['HTTP'][0]['IP'], headers=Headers, data=data+'\n', timeout=2)
-        connection_status = res.status_code    
-        while connection_status != 200:
-            rep_cnt += 1
+        try:
+            logger.debug('Data to be send: '+data)
             res = requests.post(url=connect_data['HTTP'][0]['IP'], headers=Headers, data=data+'\n', timeout=2)
-            connection_status = res.status_code 
-            if rep_cnt > 30:
-                logger.warning('Backend: Failed send the message to the rover')
-                return connection_status 
-            time.sleep(1)  
+            connection_status = res.status_code    
+            while connection_status != 200:
+                rep_cnt += 1
+                res = requests.post(url=connect_data['HTTP'][0]['IP'], headers=Headers, data=data+'\n', timeout=2)
+                connection_status = res.status_code 
+                if rep_cnt > 30:
+                    logger.warning('Backend: Failed send the message to the rover')
+                    return connection_status 
+                time.sleep(1) 
+        except requests.exceptions.RequestException as e:
+            logger.warning('Backend: HTTP-Connection to the rover lost or not possible. Trying to reconnect')
+            logger.debug(str(e))
     return connection_status
 
 def connect_http(connect_data: dict()) -> list:
     logger.info('Backend: Try initial HTTP request')
     try:
-        res = requests.post(url=connect_data['HTTP'][0]['IP'], headers=Headers, data=reqandchecksum('AT+V')+'\n', timeout=2)
+        data = reqandchecksum('AT+V')
+        logger.debug('Data to be send: '+data)
+        res = requests.post(url=connect_data['HTTP'][0]['IP'], headers=Headers, data=data+'\n', timeout=2)
         logger.debug('Status code: '+str(res.status_code))
         logger.debug('Content: '+res.text)
         if res.status_code == 200 and 'V,' in res.text:
@@ -86,6 +92,7 @@ def connect_http(connect_data: dict()) -> list:
             return [-1, 0, 0]
     except requests.exceptions.RequestException as e: 
         logger.warning('Backend: HTTP-Connection to the rover lost or not possible. Trying to reconnect')
+        logger.debug(str(e))
         datatodf.add_online_to_df_from_http(False)
         return [-1, 0, 0]
 
@@ -93,7 +100,7 @@ def get_state(connect_data: dict(), connection: list()) -> int:
     logger.info('Backend: Performing get state http-request')
     data = reqandchecksum('AT+S')
     if connection[1] == 1:
-        logger.debug('encryption true')
+        logger.debug('Encryption: true')
         try:
             encryptkey = int(connect_data['HTTP'][1]['PASSWORD'])%int(connection[2])
         except:
@@ -104,7 +111,8 @@ def get_state(connect_data: dict(), connection: list()) -> int:
         data_encrypt = [x - 126 + 31 if x>=126 else x for x in data_encrypt]
         data = ''.join(map(chr, data_encrypt))
 
-    try: 
+    try:
+        logger.debug('Data to be send: '+data) 
         res = requests.post(url=connect_data['HTTP'][0]['IP'], headers=Headers, data=data+'\n', timeout=2)
         logger.debug('Status code: '+str(res.status_code))
         logger.debug('Content: '+res.text)
@@ -119,13 +127,14 @@ def get_state(connect_data: dict(), connection: list()) -> int:
             return -1
     except requests.exceptions.RequestException as e: 
         logger.warning('Backend: HTTP-Connection to the rover lost or not possible. Trying to reconnect')
+        logger.debug(str(e))
         return -1
 
 def get_stats(connect_data: dict(), connection: list()) -> int:
     logger.info('Backend: Performing get stats http-request')
     data = reqandchecksum('AT+T')
     if connection[1] == 1:
-        logger.debug('encryption true')
+        logger.debug('Encryption: true')
         try:
             encryptkey = int(connect_data['HTTP'][1]['PASSWORD'])%int(connection[2])
         except:
@@ -137,6 +146,7 @@ def get_stats(connect_data: dict(), connection: list()) -> int:
         data = ''.join(map(chr, data_encrypt))
 
     try:
+        logger.debug('Data to be send: '+data)
         res = requests.post(url=connect_data['HTTP'][0]['IP'], headers=Headers, data=data+'\n', timeout=2)
         logger.debug('Status code: '+str(res.status_code))
         logger.debug('Content: '+res.text)
@@ -152,39 +162,5 @@ def get_stats(connect_data: dict(), connection: list()) -> int:
         
     except requests.exceptions.RequestException as e: 
         logger.warning('Backend: HTTP-Connection to the rover lost or not possible. Trying to reconnect')
+        logger.debug(str(e))
         return -1
-
-# def start_http(connect_data: dict(), connection_status: int):
-#     stats_couter = 0
-#     state_counter = 0
-#     last_call_move = False
-#     while True:
-#         if connection_status != 200:
-#             connection_status = connect_http(connect_data)
-#         else:
-#             #Get state data
-#             if state_counter == 10:
-#                 connection_status = get_state(connect_data)
-#                 state_counter = 0
-
-#             #Get stats data
-#             if stats_couter == 150:
-#                 connection_status = get_stats(connect_data)
-#                 stats_couter = 0
-#             state_counter += 1
-#             stats_couter += 1
-
-#             #Send commands to rover if there
-#             #msg_pckg = message.check() 
-#             connection_status = cmd_to_rover(connect_data)#, msg_pckg)
-
-#             time.sleep(0.4)
-
-# if __name__ == '__main__':
-#     connect_data = {"HTTP":
-#     [
-#         {"IP": "http://192.168.2.55"},
-#         {"PASSWORD": ""}
-#     ]
-# }
-#     connection_status = connect_http(connect_data)
