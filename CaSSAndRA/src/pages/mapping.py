bfed35b3279cdf4a687e8877ea246a9d132a5172
Micka41 <git@mpicaud.fr>
2023-07-07 15:49:26 +0200

    Zoom on mouse wheel

--- CaSSAndRA/src/pages/mapping.py
+++ CaSSAndRA/src/pages/mapping.py
@@ -21,7 +21,7 @@ def update_layout() -> html.Div:
                              children=[
                                  dbc.Spinner(delay_show=1000, 
                                              children=html.Div([
-                                                        dcc.Graph(id=ids.MAPPINGMAP, config= {'displaylogo': False})
+                                                        dcc.Graph(id=ids.MAPPINGMAP, config= {'displaylogo': False, 'scrollZoom': True})
                                                         ], className='map-graph'
                                                     )
                                 )
