38bc7523f127f04fb51be86f7b85aa44fe5d7011
Nick Roth <nlr06886@gmail.com>
2023-09-05 16:07:48 -0500

    Rework navbar
    This adopts a more common navigation pattern, plus shows the user where they are currently on larger screen sizes

--- CaSSAndRA/src/components/navbar.py
+++ CaSSAndRA/src/components/navbar.py
@@ -7,32 +7,34 @@ from . import ids
 
 # component
 navbar = dbc.Navbar(
-    dbc.Container(
-        [
-            dbc.NavbarBrand('CaSSAndRA',href='/'),
-            dbc.Nav(dbc.NavItem([
-                dbc.Button(id=ids.OPEN_OFFCANVAS, n_clicks=0, size='lg',class_name='bi bi-joystick', title='open remote control'),
-                dbc.Button(id=ids.BUTTONOPENMODALINFO, n_clicks=0, size='lg',class_name='bi bi-info-square ms-3 me-5', title='open infobox')
-            ]), className='ms-auto'),
-
-            dbc.NavbarToggler(id=ids.NAVBARTOGGLER, n_clicks=0),
-            dbc.Collapse(
-                dbc.Nav([
-                    dbc.NavItem(dbc.NavLink('Mapping', href='/mapping')),
-                    dbc.NavItem(dbc.NavLink('Settings', href='/settings')),
-                    #dbc.NavItem(dbc.NavLink('Taskplanner', href='/taskplanner')),
-                    #dbc.NavItem(dbc.NavLink('Statistik', href='/stats')),
-                ]),
-                id=ids.NAVBARCOLLAPSE,
-                is_open=False,
-                navbar=True,
+    [
+        dbc.NavbarToggler(id=ids.NAVBARTOGGLER, n_clicks=0, class_name="mx-1 px-2"),
+        dbc.NavbarBrand('CaSSAndRA', href='/', class_name="mx-2 flex-grow-1"),
+        dbc.Nav(dbc.NavItem([
+            dbc.Button(id=ids.OPEN_OFFCANVAS, n_clicks=0, size='lg',class_name='bi bi-joystick', title='open remote control'),
+            dbc.Button(id=ids.BUTTONOPENMODALINFO, n_clicks=0, size='lg',class_name='bi bi-info-square', title='open infobox')
+        ]), className='order-md-2 flex-grow-1 align-items-end justify-content-end'),
+        dbc.Collapse(
+            dbc.Nav([
+                dbc.NavItem(dbc.NavLink('State', href='/', active='exact')),
+                dbc.NavItem(dbc.NavLink('Mapping', href='/mapping', active='partial')),
+                #dbc.NavItem(dbc.NavLink('Taskplanner', href='/taskplanner', active='partial')),
+                dbc.NavItem(dbc.NavLink('Settings', href='/settings', active='partial')),
+                #dbc.NavItem(dbc.NavLink('Statistik', href='/stats')),
+            ],
+            pills=True,
+            class_name="mx-2",
             ),
-        ]
-    ),
-        className='mb-',
-        color='primary',
-        dark=True,
-    )
+            id=ids.NAVBARCOLLAPSE,
+            is_open=False,
+            navbar=True,
+            class_name="order-md-1 px-2 flex-grow-5 justify-content-center"
+        ),
+    ],
+    color='primary',
+    dark=True,
+    class_name="d-flex justify-content-start px-2"
+)
 
 # add callback for toggling the collapse on small screens
 @callback(
