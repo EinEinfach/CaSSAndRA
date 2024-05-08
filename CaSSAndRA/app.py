a1b5a6b23a376ccdbd6aa1d181dc7263290945a8
Alexander <sanjok@gmx.de>
2024-05-08 08:48:28 +0200

    Fix an issue where api settings or message service settings could be removed

--- CaSSAndRA/app.py
+++ CaSSAndRA/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#Version:0.109.1 Remove geopandas dependency 
+#Version:0.109.2 Fix an issue where api settings or message service setting could be removed after save and reboot
 # package imports
 import os
 import sys
